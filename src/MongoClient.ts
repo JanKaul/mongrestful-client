b60f9a5ff50cadcb1bdab5b8a0b2f568cde7db81
Jan Kaul <jankaul@mailbox.org>
2022-01-27 14:33:14 +0100

    share sessionSecret

--- src/MongoClient.ts
+++ src/MongoClient.ts
@@ -1,18 +1,55 @@
 import * as jose from "jose"
 import { Db } from "./Db";
+import { Option, some, none } from "./TsOption"
+import { match, select } from 'ts-pattern';
 
 export class MongoClient {
     url: URL
-    db: Db
-    serverPublicKey: jose.KeyLike
+    sessionSecret: Option<jose.KeyLike>
+    db: Option<Db>
     constructor(urlString) {
-        this.url = new URL(urlString);
-        let { username, password } = this.url;
-        this.url.username = this.url.password = ""
+        let url = new URL(urlString);
+        url.username = url.password = ""
+        this.url = url
+
+        this.sessionSecret = none()
+        this.db = none()
     }
-    async connect() {
-        this.serverPublicKey = await fetch(this.url.toString(), { method: "GET" })
+    async connect(username, password) {
+
+        let publicUrl = this.url
+        publicUrl.pathname = "/public_key"
+
+        const serverPublicKey = await fetch(publicUrl.toString(), { method: "GET" })
             .then(response => { return response.text() })
-            .then(text => { return jose.importSPKI(text, 'ES256') })
+            .then(text => { return jose.importSPKI(decodeURIComponent(text), 'RSA-OAEP-256') })
+
+        const { publicKey, privateKey } = await jose.generateKeyPair('RSA-OAEP-256')
+
+        const jwt = await new jose.EncryptJWT({
+            username: encodeURIComponent(username),
+            password: encodeURIComponent(password),
+            clientPublicKey: encodeURIComponent(await jose.exportSPKI(publicKey))
+        })
+            .setProtectedHeader({ alg: 'RSA-OAEP-256', enc: 'A256GCM' })
+            .setIssuedAt()
+            .encrypt(serverPublicKey)
+
+        let connectUrl = this.url
+        connectUrl.pathname = "/connect"
+
+        const secretToken = await fetch(connectUrl.toString(), {
+            method: "POST",
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: jwt
+        }).then(response => { return response.text() })
+
+        const { payload, protectedHeader } = await jose.jwtDecrypt(secretToken, privateKey)
+
+        const { secret } = payload;
+
+        this.sessionSecret = some(await jose.importJWK(JSON.parse(decodeURIComponent(secret as string)), 'A256GCM') as jose.KeyLike)
     }
 }
--- test/mongrestful.test.js
+++ test/mongrestful.test.js
@@ -3,8 +3,8 @@ import { MongoClient } from "../dist/index"
 
 describe("get publicKey", () => {
     it('', (done) => {
-        let client = new MongoClient("http://localhost:3000/public_key");
-        client.connect().then(_ => { expect(client.serverPublicKey).to.not.equal(undefined); done() })
+        let client = new MongoClient("http://localhost:3000");
+        client.connect("jan", "kaul").then(_ => { expect(client.url).to.not.equal(undefined); done() })
             .catch(x => { console.log(x); done() })
     })
 });
